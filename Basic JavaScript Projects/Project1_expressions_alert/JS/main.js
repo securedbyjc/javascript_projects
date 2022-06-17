2eba56a73628b09383c72154541ba9cfa56b9069
securedbyjc <104037936+securedbyjc@users.noreply.github.com>
2022-06-17 18:18:54 -0400

    Project 1 Expressions Alert
    Debugged js code

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,7 @@
-window.alert("Hello, world!") //assign the alert box
+window.alert("Hello, world!")               //assign the alert box
 
 var A = 'I am 46 years old ';           //Defining a variable an giving it a string value
 var B =  'and my favorite animals are cats.'; //Defining a variable an giving it a string value
 document.write(A + B); {
-}                                     //Reslut I am 46 years old and my favorite animals are cats.// 
+  // TODO document why this block is empty  //Results: I am 46 years old and my favorite animals are cats.// 
+}                                    
