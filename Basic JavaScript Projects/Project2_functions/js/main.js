8712e30b9ea42d5fe2010a18387d6064e5d9da3b
securedbyjc <104037936+securedbyjc@users.noreply.github.com>
2022-06-16 23:52:40 -0400

    Project2_functions
    Project 2 is utilizing a function that uses a "+=" operator.

---
+++ Basic JavaScript Projects/Project2_functions/js/main.js
@@ -0,0 +1,5 @@
+var A = 'I am 46 years old ';           //Defining a variable an giving it a string value
+var B =  'and my favorite animals are cats.'; //Defining a variable an giving it a string value
+
+
+document.write(A += B)                       //Utilizing the " += " operator within this method
