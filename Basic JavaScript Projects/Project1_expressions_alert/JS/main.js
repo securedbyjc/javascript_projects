4a27e2199576bfc648a4dc9335cc0c04d86ac7ef
securedbyjc <104037936+securedbyjc@users.noreply.github.com>
2022-06-17 17:08:21 -0400

    Updated Project 1 Expressions Alert
    Assessed file path and made corrections.

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,4 +2,5 @@ window.alert("Hello, world!") //assign the alert box
 
 var A = 'I am 46 years old ';           //Defining a variable an giving it a string value
 var B =  'and my favorite animals are cats.'; //Defining a variable an giving it a string value
-document.write(A + B)
+document.write(A + B); {
+}                                     //Reslut I am 46 years old and my favorite animals are cats.// 
