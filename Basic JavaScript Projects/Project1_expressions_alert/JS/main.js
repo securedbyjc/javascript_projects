dab65f00f4ec1fbec5e0f66617c25b371355fa2a
securedbyjc <104037936+securedbyjc@users.noreply.github.com>
2022-06-16 22:56:35 -0400

    Updated "Basic JavaScript Projects” folder
    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,13 +1,5 @@
 window.alert("Hello, world!") //assign the alert box
 
-let myPet = "cats";
-document.write('My favorite animals are ' + myPet + '.');  //My Favorite aninimals are cats. 
-
-let myAge = 46;
-document.write("I am " + myAge + " years old.");  // I am 46 years old.
-
-let x = 5;        // assign the value 5 to x
-let y = 2;        // assign the value 5 to x
-let z = x + y;    // assign the value 7 to z (5 + 2)
-document.write(z);
-
+var A = 'I am 46 years old ';           //Defining a variable an giving it a string value
+var B =  'and my favorite animals are cats.'; //Defining a variable an giving it a string value
+document.write(A + B)
