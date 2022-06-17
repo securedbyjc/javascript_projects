199b025c7d8e952d89df234cbfb565e1f8706e1d
securedbyjc <104037936+securedbyjc@users.noreply.github.com>
2022-06-17 15:03:36 -0400

    Updated Project 2 Functions

--- Basic JavaScript Projects/Project2_functions/js/main.js
+++ Basic JavaScript Projects/Project2_functions/js/main.js
@@ -1,5 +1,7 @@
 var A = 'I am 46 years old ';           //Defining a variable an giving it a string value
 var B =  'and my favorite animals are cats.'; //Defining a variable an giving it a string value
 
+document.write(A += B); {
+  //Utilizing the " += " operator within this method
 
-document.write(A += B)                       //Utilizing the " += " operator within this method
+}  
