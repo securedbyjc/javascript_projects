9b9ac9541cdd4b89d654a80915b68873c6eeae4d
securedbyjc <104037936+securedbyjc@users.noreply.github.com>
2022-06-22 16:16:08 -0400

    Updated Project 5

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,36 +1,36 @@
  //Typeof Opertor Sample: string and expres//
  function typeof_Function() {
-    var result = typeof("book");
+    var result = document.write(typeof("book"));
     document.getElementById("One").innerHTML = result;
 }
  //Type Coercion = a expression combined a string and a number.//
  function function_Expression() {
-    var result = document.write("number" + 15 + 3);
+    var result = "number" + 15 + 3;
     document.getElementById("Two").innerHTML = result;
 }
 // == is used to that a comparison should be made//
 function doubleeq_Function() {
-    var result = document.write([1,2,3] == [1,2,4]);
+    var result = ([1,2,3] == [1,2,4]);
     document.getElementById("Three").innerHTML = result;
 }
 // === tells the computer to check whether the two pieces of data are equal in value and TYPE.//
 function tripleeq_Function() {
-    var result = document.write(15 === (10 + 5));
+    var result = 15 === (10 + 5);
     document.getElementById("Four").innerHTML = result;
 }
 //The && is 1 of 3 Boolean logical operators and determines the logic between values or variables//
 function boolean_Function() {
-    var result =  document.write(125 > 123);
+    var result =  125 > 123;
     document.getElementById("Five").innerHTML = result;
 }
 //The && is 1 of 3 Boolean logical operators and determines the logic between values or variables//
 function boolean2_Function() {
-    var result =  document.write(5 > 2 && 10 > 4);
+    var result =  5 > 2 && 10 > 4;
     document.getElementById("Six").innerHTML = result;
 }
 //The && is 1 of 3 Boolean logical operators and determines the logic between values or variables//
 function boolean3_Function() {
-    var result = document.write(7 > 20 || 20 > 4);
+    var result = 7 > 20 || 20 > 4;
     document.getElementById("Seven").innerHTML = result;
 }
 //The ! (not) operator checks whether or not something is true.
