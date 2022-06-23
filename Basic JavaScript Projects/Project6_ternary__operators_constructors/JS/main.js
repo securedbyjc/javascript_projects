16e18e8d8ae7ab71137d9be244cbd22ec5006538
securedbyjc <104037936+securedbyjc@users.noreply.github.com>
2022-06-23 17:03:28 -0400

    Project 6 Ternary Operators & Constructors
    Wrote a function using HTML and JavaScript that utilizes a ternary operation using input from the browser.
    
    Wrote a constructor function that uses the “new” and “this” keywords.
    
    Created a function to display the results of the constructor function within an HTML element.
    
    Wrote a nested function.
    
    Write comments throughout your JavaScript explaining your code

---
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,49 @@
+function Grading_Function()  {
+	var Grades, Good_enough;
+	Grades = document.getElementById("Grades").value;
+	Good_enough = (Grades > 3.00) ? "Your GPA does not meet our requirement":"Your GPA does meet our requirement";
+	document.getElementById("Pass").innerHTML = Good_enough + " to be accepted into the program.";
+}
+// Constructor function for Person objects
+function Person(first, last, age, eye) {
+	this.firstName = first;
+	this.lastName = last;
+	this.age = age;
+	this.eyeColor = eye;
+}
+
+// Create a Person object
+const myWife = new Person("Sally", "Wong", 33, "brown "); 
+
+// Display Eyes
+function functionEyes() {
+	var result = + myWife.eyeColor + "." + ".";
+document.getElementById("Eyes").innerHTML =
+"My wife has " + myWife.eyeColor + " " + " eyes."; 
+}
+
+// Constructor Function
+function Sofa(Make, Style, Color) {
+	this.Sofa_Make = Make;
+	this.Sofa_Style = Style;
+	this.Sofa_Color = Color;
+}
+var Sandy = new Sofa("Ashley's Furniture", "Sectional", "Brown");
+var Randy = new Sofa("Basset", "Chesterfield", "White");
+var Mandy = new Sofa("Century", "Lawson", "Grey");
+function myFunction() { 
+	document.getElementById("Keywords_and_Constructors").innerHTML = //Code to display constructor in HTML
+	"Sandy bought a " + Sandy.Sofa_Color + "-colored " + Sandy.Sofa_Style +
+	" manufactured by " + Sandy.Sofa_Make;
+}
+
+//  Nested Function
+function count_Function()  {
+	document.getElementById("Counting").innerHTML = Count ();
+	function Count() {
+		var Starting_point = 15;
+		function Plus_One()  {Starting_point += 1;}
+		Plus_One();
+		return Starting_point;
+	}
+}
